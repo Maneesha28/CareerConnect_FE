4d6b57d36ff74086220e468528ca00537bf7a628
Maneesha Rani Saha <1805076@ugrad.cse.buet.ac.bd>
2023-08-28 00:52:34 +0600

    env configuration updated

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apikey : process.env.REACT_APP_FIREBASE_API_KEY,
+    apiKey : process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain : process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId : process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket : process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
